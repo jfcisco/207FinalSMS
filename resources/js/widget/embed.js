94303f7f6001e3311ab716a2f6f65ef3f0adecd2
ercsanchez <3739617-ercsanchez@users.noreply.gitlab.com>
2022-05-28 16:06:13 +0800

    fix errors on undefined html tags

--- resources/js/widget/embed.js
+++ resources/js/widget/embed.js
@@ -4,15 +4,15 @@
 
 import { Tawk } from './Tawk';
 
-/* 
-    Notes: The strings below are filled out by the Laravel application. 
-    
-    For more details, consult the App\Http\Controllers\WidgetController::generateScript() function. 
+/*
+    Notes: The strings below are filled out by the Laravel application.
+
+    For more details, consult the App\Http\Controllers\WidgetController::generateScript() function.
 */
 
 const tawk = new Tawk({
     baseUrl: "<?php echo $baseUrl; ?>",
-    hasScheduledAvailability: ("<?php echo $hasScheduledAvailability; ?>" === "true"),
-    availabilityStartTime: new Date("<?php echo $availabilityStartTime; ?>"),
-    availabilityEndTime: new Date("<?php echo $availabilityEndTime; ?>"),
+    // hasScheduledAvailability: ("<?php echo $hasScheduledAvailability; ?>" === "true"),
+    // availabilityStartTime: new Date("<?php echo $availabilityStartTime; ?>"),
+    // availabilityEndTime: new Date("<?php echo $availabilityEndTime; ?>"),
 });
